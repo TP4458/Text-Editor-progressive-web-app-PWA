1ea08e815166dd7afdfb473f68378eb04cd8100c
Tomasz Pawlikowski <tp4458@gmail.com>
2024-02-16 23:18:08 +0000

    configured webpack plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,20 +11,44 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //HtmlWebpackPlugin will generate the HTML file and inject the bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor',
+      }),
+      //injects custom SW for precaching
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        name: 'Text Editor',
+        short_name: 'JATE',
+        description: 'note taking PWA',
+        background_color: '#272822',
+        theme_color: '#272822',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
-      rules: [
-        
-      ],
+      rules: [],
     },
   };
 };

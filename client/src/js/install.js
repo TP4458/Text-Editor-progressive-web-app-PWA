36b3838eeb75f21d2e71722083efdfe93863499f
Tomasz Pawlikowski <tp4458@gmail.com>
2024-02-16 23:47:41 +0000

    clickhandler in butInstall element

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,15 +1,26 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// An event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
   window.deferredPrompt = event;
   butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+//  Implements a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    ReadableStreamDefaultController;
+  }
+  //display prompt
+  promptEvent.prompt();
+  // Reset the deferred prompt variable, prompt() can only be called once.
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});

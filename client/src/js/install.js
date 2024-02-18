e2fdf5e117ee4f30f255374d5130983f6b5f3d98
Tomasz Pawlikowski <tp4458@gmail.com>
2024-02-18 10:45:37 +0000

    hiddexn install button in installed app

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,22 +4,22 @@ const butInstall = document.getElementById('buttonInstall');
 // An event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
-  window.deferredPrompt = event;
   butInstall.classList.toggle('hidden', false);
+  window.deferredPrompt = event;
 });
 
 //  Implements a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
   const promptEvent = window.deferredPrompt;
   if (!promptEvent) {
-    ReadableStreamDefaultController;
+    return;
   }
+  butInstall.classList.toggle('hidden', true);
   //display prompt
   promptEvent.prompt();
   // Reset the deferred prompt variable, prompt() can only be called once.
-  window.deferredPrompt = null;
 
-  butInstall.classList.toggle('hidden', true);
+  window.deferredPrompt = null;
 });
 
 // TODO: Add an handler for the `appinstalled` event
